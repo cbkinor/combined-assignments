146e3335eb2e3aa1ab0e3782e39385290a871da3
Chris Kuiper <chris.kuiper@gmail.com>
2016-10-10 18:31:13 -0500

    add ip address and port for server

--- 5-socket-io-serialization/src/main/java/com/cooksys/ftd/assignments/socket/Utils.java
+++ 5-socket-io-serialization/src/main/java/com/cooksys/ftd/assignments/socket/Utils.java
@@ -1,8 +1,13 @@
 package com.cooksys.ftd.assignments.socket;
 
 import com.cooksys.ftd.assignments.socket.model.Config;
+import com.cooksys.ftd.assignments.socket.model.Student;
+
+import java.io.File;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 
 /**
  * Shared static methods to be used by both the {@link Client} and {@link Server} classes.
@@ -11,9 +16,10 @@ public class Utils {
     /**
      * @return a {@link JAXBContext} initialized with the classes in the
      * com.cooksys.socket.assignment.model package
+     * @throws JAXBException 
      */
-    public static JAXBContext createJAXBContext() {
-        return null; // TODO
+    public static JAXBContext createJAXBContext() throws JAXBException {
+    	return JAXBContext.newInstance(Student.class, Config.class);
     }
 
     /**
@@ -22,8 +28,12 @@ public class Utils {
      * @param configFilePath the file path to the config.xml file
      * @param jaxb the JAXBContext to use
      * @return a {@link Config} object that was read from the config.xml file
+     * @throws JAXBException 
      */
-    public static Config loadConfig(String configFilePath, JAXBContext jaxb) {
-        return null; // TODO
+    public static Config loadConfig(String configFilePath, JAXBContext jaxb) throws JAXBException {
+    	Unmarshaller unmarshaller = jaxb.createUnmarshaller();
+       	File file = new File(configFilePath);
+       	Config result =(Config)unmarshaller.unmarshal(file);
+    	return result;
     }
 }
